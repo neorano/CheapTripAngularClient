b2231359f2545fc5f810f853ee9a5f2a1fd2dc4a
neorano <60232733+neorano@users.noreply.github.com>
2021-01-29 18:35:25 +0200

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DataTableComponent } from './authors/data-table/data-table.component';
+import { HeaderComponent } from './header/header/header.component';
+import { LandingComponent } from './landing/landing.component';
+import { LinksComponent } from './links/links.component';
+import { NoPageComponent } from './no-page/no-page.component';
+
+const routes: Routes = [
+  { path: 'landing', component: LandingComponent },
+//  { path: 'main',component: HeaderComponent},
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  {
+    path: 'search',
+    //   canActivate: [SearchResultGuard],
+    loadChildren: () =>
+      import('./trip-direction/trip-direction.module').then(
+        (m) => m.TripDirectionModule
+      ),
+  },
+  {
+    path: 'links',component: LinksComponent
+   
+  },
+  /*  {
+    path: '',
+    component: HeaderComponent,
+    children: [
+      {
+        path: 'search',
+        //   canActivate: [SearchResultGuard],
+        loadChildren: () =>
+          import('./trip-direction/trip-direction.module').then(
+            (m) => m.TripDirectionModule
+          ),
+      },
+    ],
+  }, */
+
+  { path: '**', component: NoPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  url68: 'http://3.18.167.68:8080/',        //basic
+  // url104: 'http://18.158.89.230:8080/',  //basic 
+  url104: 'http://35.238.107.151:8080/',    //my
+};
